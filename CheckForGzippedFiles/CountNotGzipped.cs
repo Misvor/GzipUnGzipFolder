5a5386b7fb922b97653133d2c9d4a1edf41523a9
Mikhail Voronkov <mikhail.voronkov@nordclan.com>
2025-01-09 16:17:48 +0300

    refactor and move to newer .net

--- CheckForGzippedFiles/CountNotGzipped.cs
+++ CheckForGzippedFiles/CountNotGzipped.cs
@@ -1,109 +1,110 @@
 ﻿using System;
 using System.IO;
 
-namespace CheckForGzippedFiles
+namespace CountNotGzipped
 {
-	class CountNotGzipped
-	{
-		static void Main(string[] args)
-		{
-			var path = "/var/lib/docker/volumes/container_name/_data";
+    class CountNotGzipped
+    {
+        static void Main(string[] args)
+        {
+            var path = "/var/lib/docker/volumes/container_name/_data";
 
-			Console.WriteLine($"Is \"{path}\" the right directory? \n y/N");
+            Console.WriteLine($"Is \"{path}\" the right directory? \n y/N");
 
-			var choice = Console.ReadLine();
-			if (choice != "y" || !Directory.Exists(path))
-			{
-				if (choice == "y")
-				{
-					Console.WriteLine($"Specified directory is not valid!");
-				}
-				path = string.Empty;
-				while (string.IsNullOrWhiteSpace(path))
-				{
-					path = ChoseAnotherPath();
-				}
-			}
+            var choice = Console.ReadLine();
+            if (choice != "y" || !Directory.Exists(path))
+            {
+                if (choice == "y")
+                {
+                    Console.WriteLine($"Specified directory is not valid!");
+                }
 
-			Console.WriteLine($"Specified directory is valid");			
+                path = string.Empty;
+                while (string.IsNullOrWhiteSpace(path))
+                {
+                    path = ChoseAnotherPath();
+                }
+            }
 
-			if (AskForConfirm())
-			{
-				var count = CountCompressed(path);
-				Console.WriteLine($"{count.Gzipped} Files compressed! \n {count.NotGzipped} Files not compressed \n {count.ZeroSize} Files of 0 size");
-			}
-		}
+            Console.WriteLine($"Specified directory is valid");
 
-		private static string ChoseAnotherPath()
-		{
-			Console.WriteLine("Enter directory path:");
-			var chosenPath = Console.ReadLine();
-			if (!string.IsNullOrWhiteSpace(chosenPath))
-			{
-				if (Directory.Exists(chosenPath))
-				{
-					return chosenPath;
-				}
-			}
+            if (!AskForConfirm()) return;
+            var count = CountCompressed(path);
+            Console.WriteLine(
+                $"{count.Gzipped} Files compressed! \n {count.NotGzipped} Files not compressed \n {count.ZeroSize} Files of 0 size");
+        }
 
-			Console.WriteLine("Directory is not valid.");
-			return string.Empty;
-		}
-		private static bool AskForConfirm()
-		{
-			Console.WriteLine("\n Do you want to count not compressed files in this directory? \n y/N");
-			var result = Console.ReadLine();
-			return result == "y";
-		}
+        private static string ChoseAnotherPath()
+        {
+            Console.WriteLine("Enter directory path:");
+            var chosenPath = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(chosenPath))
+            {
+                if (Directory.Exists(chosenPath))
+                {
+                    return chosenPath;
+                }
+            }
 
-		private static CountResult CountCompressed(string path)
-		{
-			var directory = new DirectoryInfo(path);			
-			var gzipped = 0;
-			var notGzipped = 0;
-			var zeroSize = 0;
-			foreach (var file in directory.GetFiles())
-			{
-				var fileBytes = File.ReadAllBytes(file.FullName);
-				if (fileBytes.Length != 0)
-				{
-					if (!FileGzipped(fileBytes))
-					{
-						notGzipped++;
-					}
-					else
-					{
-						gzipped++;
-					}
-				}
-				else
-				{
-					zeroSize++;
-					Console.WriteLine($"{file.Name} Size is 0. Skipping");
-				}
-			}
-			return new CountResult()
-			{
-				NotGzipped = notGzipped,
-				Gzipped = gzipped,
-				ZeroSize = zeroSize
-			};			
-		}
+            Console.WriteLine("Directory is not valid.");
+            return string.Empty;
+        }
 
-		private static bool FileGzipped(byte[] fileBytes)
-		{
-			// GZIP file format states the first 2 bytes of the file are '\x1F' and '\x8B'
-			return (fileBytes[0] == 0x1f) && (fileBytes[1] == 0x8b);
+        private static bool AskForConfirm()
+        {
+            Console.WriteLine("\n Do you want to count not compressed files in this directory? \n y/N");
+            var result = Console.ReadLine();
+            return result == "y";
+        }
 
-		}		
+        private static CountResult CountCompressed(string path)
+        {
+            var directory = new DirectoryInfo(path);
+            var gzipped = 0;
+            var notGzipped = 0;
+            var zeroSize = 0;
+            foreach (var file in directory.GetFiles())
+            {
+                var fileBytes = File.ReadAllBytes(file.FullName);
+                if (fileBytes.Length != 0)
+                {
+                    if (!FileGzipped(fileBytes))
+                    {
+                        notGzipped++;
+                    }
+                    else
+                    {
+                        gzipped++;
+                    }
+                }
+                else
+                {
+                    zeroSize++;
+                    Console.WriteLine($"{file.Name} Size is 0. Skipping");
+                }
+            }
 
-	}
-	class CountResult
-	{
-		public int Gzipped { get; set; }
+            return new CountResult()
+            {
+                NotGzipped = notGzipped,
+                Gzipped = gzipped,
+                ZeroSize = zeroSize
+            };
+        }
 
-		public int NotGzipped { get; set; }
+        private static bool FileGzipped(byte[] fileBytes)
+        {
+            // GZIP file format states the first 2 bytes of the file are '\x1F' and '\x8B'
+            return (fileBytes[0] == 0x1f) && (fileBytes[1] == 0x8b);
+        }
+    }
 
-		public int ZeroSize { get; set; }
-	}
+    class CountResult
+    {
+        public int Gzipped { get; set; }
+
+        public int NotGzipped { get; set; }
+
+        public int ZeroSize { get; set; }
+    }
 }
--- GzipNotGzippedFiles/Program.cs
+++ GzipNotGzippedFiles/Program.cs
@@ -1,169 +1,144 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
-using System.Threading.Tasks;
 
 namespace GzipNotGzippedFiles
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var path = "/var/lib/docker/volumes/container_name/_data";
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "/var/lib/docker/volumes/container_name/_data";
 
-			Console.WriteLine($"Is \"{path}\" the right directory? \n y/N");
+            Console.WriteLine($"Is \"{path}\" the right directory? \n y/N");
 
-			var choice = Console.ReadLine();
-			if (choice != "y" || !Directory.Exists(path))
-			{
-				if (choice == "y")
-				{
-					Console.WriteLine($"Specified directory is not valid!");
-				}
-				path = string.Empty;
-				while (string.IsNullOrWhiteSpace(path))
-				{
-					path = ChoseAnotherPath();
-				}
-			}
+            var choice = Console.ReadLine();
+            if (choice != "y" || !Directory.Exists(path))
+            {
+                if (choice == "y")
+                {
+                    Console.WriteLine($"Specified directory is not valid!");
+                }
 
-			Console.WriteLine($"Specified directory is valid");
+                path = string.Empty;
+                while (string.IsNullOrWhiteSpace(path))
+                {
+                    path = ChoseAnotherPath();
+                }
+            }
 
-			if (choice == "I want to decompress!")
-			{
-				Console.WriteLine("Your wish is granted");
-				DecompressFilesInFolder(path);
-				Console.WriteLine("Files decompressed!");
-			}
+            Console.WriteLine($"Specified directory is valid");
 
-			if (AskForConfirm())
-			{
-				CompressFilesInFolder(path);
-				Console.WriteLine("Files compressed!");
-			}
-		}
-		private static string ChoseAnotherPath()
-		{
-			Console.WriteLine("Enter directory path:");
-			var chosenPath = Console.ReadLine();
-			if (!string.IsNullOrWhiteSpace(chosenPath))
-			{
-				if (Directory.Exists(chosenPath))
-				{
-					return chosenPath;
-				}
-			}
+            if (choice == "I want to decompress!")
+            {
+                Console.WriteLine("Your wish is granted");
+                DecompressFilesInFolder(path);
+                Console.WriteLine("Files decompressed!");
+            }
 
-			Console.WriteLine("Directory is not valid.");
-			return string.Empty;
-		}
-		private static bool AskForConfirm()
-		{
-			Console.WriteLine("\n Are you sure you want to COMPRESS all files in this directory? \n y/N");
-			var result = Console.ReadLine();
-			return result == "y";
-		}
+            if (!AskForConfirm()) return;
+            CompressFilesInFolder(path);
+            Console.WriteLine("Files compressed!");
+        }
 
-		private static void CompressFilesInFolder(string path)
-		{
-			var directory = new DirectoryInfo(path);
-			if (directory.Exists)
-			{
-				foreach (var file in directory.GetFiles())
-				{
-					var fileBytes = File.ReadAllBytes(file.FullName);
-					if (fileBytes.Length != 0)
-					{
-						if (!FileGzipped(fileBytes))
-						{
-							var fileData = file.OpenRead();
-							byte[] zippedBytes = Compress(fileData);
-							fileData.Dispose();
-							if (zippedBytes.Length != 0)
-							{
-								var compressedFile = File.Create(file.FullName);
-								compressedFile.Write(zippedBytes, 0, zippedBytes.Length);
-							}
-							else
-							{
-								Console.WriteLine($"could not compress {file.Name} Skipping");
-							}
-						}
-					}
-					else
-					{
-						Console.WriteLine($"{file.Name} is corrupted! Skipping");
-					}
+        private static string ChoseAnotherPath()
+        {
+            Console.WriteLine("Enter directory path:");
+            var chosenPath = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(chosenPath))
+            {
+                if (Directory.Exists(chosenPath))
+                {
+                    return chosenPath;
+                }
+            }
 
-				}
-			}
-		}
+            Console.WriteLine("Directory is not valid.");
+            return string.Empty;
+        }
 
-		private static byte[] Compress(Stream data)
-		{
-			using (var compressedStream = new MemoryStream())
-			{
-				using (var zipStream = new GZipStream(compressedStream, CompressionLevel.Optimal))
-				{
-					data.CopyTo(zipStream);
-					zipStream.Close();
-					var result = compressedStream.ToArray();
-					compressedStream.Dispose();
-					return result;
-				}
-			}
-		}
+        private static bool AskForConfirm()
+        {
+            Console.WriteLine("\n Are you sure you want to COMPRESS all files in this directory? \n y/N");
+            var result = Console.ReadLine();
+            return result == "y";
+        }
 
-		private static bool FileGzipped(byte[] fileBytes)
-		{
-			// GZIP file format states the first 2 bytes of the file are '\x1F' and '\x8B'
-			return (fileBytes[0] == 0x1f) && (fileBytes[1] == 0x8b);
+        private static void CompressFilesInFolder(string path)
+        {
+            var directory = new DirectoryInfo(path);
+            if (!directory.Exists) return;
+            foreach (var file in directory.GetFiles())
+            {
+                var fileBytes = File.ReadAllBytes(file.FullName);
+                if (fileBytes.Length != 0)
+                {
+                    if (FileGzipped(fileBytes)) continue;
+                    var fileData = file.OpenRead();
+                    var zippedBytes = Compress(fileData);
+                    fileData.Dispose();
+                    if (zippedBytes.Length != 0)
+                    {
+                        var compressedFile = File.Create(file.FullName);
+                        compressedFile.Write(zippedBytes, 0, zippedBytes.Length);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"could not compress {file.Name} Skipping");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{file.Name} is corrupted! Skipping");
+                }
+            }
+        }
 
-		}
+        private static byte[] Compress(Stream data)
+        {
+            using var compressedStream = new MemoryStream();
+            using var zipStream = new GZipStream(compressedStream, CompressionLevel.Optimal);
+            data.CopyTo(zipStream);
+            zipStream.Close();
+            var result = compressedStream.ToArray();
+            return result;
+        }
 
-		#region Decompress
-		private static void DecompressFilesInFolder(string path)
-		{
-			var directory = new DirectoryInfo(path);
-			if (directory.Exists)
-			{
-				foreach (var file in directory.GetFiles())
-				{
-					var fileBytes = File.ReadAllBytes(file.FullName);
-					if (FileGzipped(fileBytes))
-					{
-						var originalBytes = Decompress(fileBytes);
-						var originalFile = File.Create(file.FullName);
-						originalFile.Write(originalBytes, 0, originalBytes.Length);
-					}
-				}
-			}
-		}
+        private static bool FileGzipped(byte[] fileBytes)
+        {
+            // GZIP file format states the first 2 bytes of the file are '\x1F' and '\x8B'
+            return (fileBytes[0] == 0x1f) && (fileBytes[1] == 0x8b);
+        }
 
-		private static byte[] Decompress(byte[] data)
-		{
-			using (var compressedStream = new MemoryStream(data))
-			{
-				using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
-				{
-					var resultStream = new MemoryStream();
+        #region Decompress
 
-					zipStream.CopyTo(resultStream);
-					resultStream.Seek(0, SeekOrigin.Begin);
-					var result = resultStream.ToArray();
-					resultStream.Dispose();
-					if (result.Length != 0)
-					{
-						return result;
-					}
-					else
-					{
-						return data;
-					}
-				}
-			}
-		}
-		#endregion
+        private static void DecompressFilesInFolder(string path)
+        {
+            var directory = new DirectoryInfo(path);
+            if (!directory.Exists) return;
+            foreach (var file in directory.GetFiles())
+            {
+                var fileBytes = File.ReadAllBytes(file.FullName);
+                if (!FileGzipped(fileBytes)) continue;
+                var originalBytes = Decompress(fileBytes);
+                var originalFile = File.Create(file.FullName);
+                originalFile.Write(originalBytes, 0, originalBytes.Length);
+            }
+        }
 
-	}
+        private static byte[] Decompress(byte[] data)
+        {
+            using var compressedStream = new MemoryStream(data);
+            using var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress);
+            var resultStream = new MemoryStream();
+
+            zipStream.CopyTo(resultStream);
+            resultStream.Seek(0, SeekOrigin.Begin);
+            var result = resultStream.ToArray();
+            resultStream.Dispose();
+            return result.Length != 0 ? result : data;
+        }
+
+        #endregion
+    }
 }
